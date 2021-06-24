8288bf361ad43c7c8b49785e84cc654e2aedaf2b
Alican Demirtas <demirtaa@legal.regn.net>
2021-06-24 19:28:23 +0100

    Test Running: Setup and Teardown

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,15 @@
 describe('The question list', () => {
+
+  beforeEach(() => console.log('Before each!'));
+
+  beforeAll(() => console.log('Before all!'));
+
+  afterEach(() => console.log('After each!'));
+
+  afterAll(() => console.log('After all!'));
+
   it('should display a list of items', () => {
 
   });
+
 });
--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -2,4 +2,5 @@ describe('The Question Detail Component', () => {
   it('should not regress', () => {
     expect(40 + 2).toEqual(42);
   });
+
 });
