12cd0c03a3d6e6eda91fd324eab25b7880aa8cdd
Alican Demirtas <demirtaa@legal.regn.net>
2021-06-25 15:35:09 +0100

    Mocking: Functions and Modules

---
+++ __mocks__/isomorphic-fetch.js
@@ -0,0 +1,4 @@
+let __value;
+const isomorphicFetch = jest.fn(() => __value);
+isomorphicFetch.__setValue = v => __value = v;
+export default isomorphicFetch;
--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -3,5 +3,4 @@ describe('The Question Detail Component', () => {
   it('should not regress', () => {
     expect(40 + 2).toEqual(42);
   });
-
 });
